fe15fc1477f7875e776a9982bdc520c0af9b0d85
angelica <angelica.cavero@gmail.com>
2022-09-06 21:39:22 +0200

    useState added to form

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,7 +1,17 @@
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-export default function NewMeetupForm() {
+export default function NewMeetupForm({
+  title,
+  image,
+  address,
+  description,
+  onChangeTitle,
+  onChangeImage,
+  onChangeAddress,
+  onChangeDescription
+
+}) {
   function submitHandler(event) {
     event.preventDefault();
   }
@@ -11,19 +21,19 @@ export default function NewMeetupForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" value={title} onChange={onChangeTitle} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" value={image} onChange={onChangeImage} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" value={address} onChange={onChangeAddress} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea id="description" value={description} onChange={onChangeDescription} required rows="5"></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,40 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import React, { useState }  from 'react';
 
 export default function NewMeetupsPage() {
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
+  
+
+  const onChangeTitle = event => {
+    setTitle(event.target.value);
+  };
+  const onChangeImage = event => {
+    setImage(event.target.value);
+  };
+  const onChangeAddress = event => {
+    setAddress(event.target.value);
+  };
+  const onChangeDescription = event => {
+    setDescription(event.target.value);
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm 
+        title={title}
+        image={image}
+        address={address}
+        description={description}
+        onChangeTitle={onChangeTitle}
+        onChangeImage={onChangeImage}
+        onChangeAddress={onChangeAddress}
+        onChangeDescription={onChangeDescription}
+      />
     </section>
   );
 }
