a21e66845595ad2bb05b5e903c956beabe4b33cb
angelica <angelica.cavero@gmail.com>
2022-09-07 23:23:30 +0200

    header animation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Meetups } from '../../context/MeetupsContext';
+import { useScrollDirection } from '../../util-hooks/useScrollDirection';
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
 
   const { favItems } = useContext(Meetups);
+
+  const scrollDirection = useScrollDirection();
+  
   return (
+    <div className={`${ scrollDirection === "down" ? classes.stickyHeaderDown : classes.stickyHeader}`}>
     <header className={classes.header} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
@@ -28,5 +33,6 @@ export default function MainNavigation() {
         </ul>
       </nav>
     </header>
+      </div>
   );
 }
