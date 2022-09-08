40e1540af47b9473fc219fe265f4896773749016
angelica <angelica.cavero@gmail.com>
2022-09-08 13:46:54 +0200

    working tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable testing-library/await-async-query */
 /* eslint-disable testing-library/no-debugging-utils */
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import App from "./App";
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
@@ -11,20 +11,22 @@ import Layout from "./components/layout/Layout";
  * @returns {ShallowWrapper}
  */
 const setup = () => shallow(<App />);
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test]='${val}'`);
 
 test("renders App without crashing", () => {
   const wrapper = setup();
-  //console.log(wrapper.debug());
+   //console.log(wrapper.debug());
   expect(wrapper.exists()).toBe(true);
 });
 
 test("renders the navigation component", () => {
-  const wrapper = setup();
-  expect(wrapper.find(MainNavigation).length).toBe(1);
+  const wrapper = setup(<MainNavigation/>);
+
+  expect(wrapper.length).toBe(1);
 });
 
 test("renders the Layout component", () => {
-  const wrapper = setup();
-  expect(wrapper.find(Layout).length).toBe(1);
+
+  const wrapper = setup(<Layout/>);
+  
+  expect(wrapper.length).toBe(1);
 });
--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -1,8 +1,17 @@
 /* eslint-disable testing-library/no-debugging-utils */
 import { shallow } from "enzyme";
+import { Meetups } from "../../context/MeetupsContext";
 import MeetupItem from "./MeetupItem";
 
+function renderMeetUpItem() {
+  return (
+    <Meetups.Provider>
+        <MeetupItem />
+    </Meetups.Provider>
+  );
+}
+
 test("<MeetupItem/> renders without crashing", () => {
-  const wrapper = shallow(<MeetupItem />);
+  const wrapper = shallow(renderMeetUpItem());
   expect(wrapper.exists()).toBe(true);
 });
