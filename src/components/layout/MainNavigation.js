3ad7b5c67f3146b43b391ada1aea95fe5700dd94
angelica <angelica.cavero@gmail.com>
2022-09-08 18:12:19 +0200

    fix

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -12,27 +12,27 @@ export default function MainNavigation() {
   
   return (
     <div className={`${ scrollDirection === "down" ? classes.stickyHeaderDown : classes.stickyHeader}`}>
-    <header className={classes.header} data-test="navigation-header">
-      <div className={classes.logo}>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">
-              All Meetups
-            </Link> 
-          </li>
-          <li>
-            <Link to="/new-meetup">Add New Meetup</Link>
-          </li>
-          <li>
-            <Link to="/favorites">
-              My Favorites
-              <span className={classes.badge}>{favItems}</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-      </div>
+      <header className={classes.header} data-test="navigation-header">
+        <div className={classes.logo}>React Meetups</div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                All Meetups
+              </Link> 
+            </li>
+            <li>
+              <Link to="/new-meetup">Add New Meetup</Link>
+            </li>
+            <li>
+              <Link to="/favorites">
+                My Favorites
+                <span className={classes.badge}>{favItems}</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
